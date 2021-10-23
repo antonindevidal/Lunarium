c947a219404e3dd3b21057ea0361ce70a8de74eb
HolyBlue <hugobarrandon@gmail.com>
2021-10-23 14:57:54 +0200

    Ajout gameover colisition planete

--- source/source/Vaisseau/Vaisseau.gd
+++ source/source/Vaisseau/Vaisseau.gd
@@ -1,9 +1,9 @@
-extends StaticBody2D
+extends KinematicBody2D
 
 
 var is_moving = false;
 var velocity = Vector2.ZERO;
-var speed = 10;
+var speed = 500;
 var on_screen=true;
 
 # Called when the node enters the scene tree for the first time.
@@ -15,11 +15,13 @@ func _process(_delta):
 		if Input.is_action_just_released("launch"):
 			launch(get_global_mouse_position())
 		if is_moving:
-			position = position + velocity
+			move_and_slide(velocity)
 		else:
 			look_at(get_global_mouse_position())
 		if not get_node("notifier").is_on_screen():
 			on_screen = false
+		for i in get_slide_count():
+			get_parent().get_parent().gameOver()
 func launch(dest):
 	velocity = (dest - position).normalized() * speed;
 	is_moving = true
