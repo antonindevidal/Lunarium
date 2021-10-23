0cb26ed911f314b72d90c3b59a93fe4cc568f3e0
HolyBlue <hugobarrandon@gmail.com>
2021-10-23 14:48:34 +0200

    Ajout rapide ecran start

--- source/source/Manageurs/ManageurNiveau.gd
+++ source/source/Manageurs/ManageurNiveau.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 var arrayNiveau = [];
 var currentNiveau = -1;
+var is_playing = false;
 
 var countTMP = 120;
-
+var countRun = 30;
 var Vaisseau_on_screen = true;
 # Called when the node enters the scene tree for the first time.
 func _init():
@@ -16,15 +17,17 @@ func _ready():
 	pass;
 	
 func _process(_delta):
-	if countTMP>0:
-		countTMP = countTMP -1;
-	if countTMP == 0 :
-		nextNiveau();
-		countTMP = -1;
-	
-	Vaisseau_on_screen = (get_node("NiveauTemplate").get_node("Vaisseau")).is_on_screen()
-	if not Vaisseau_on_screen:
-		gameOver()
+	is_playing = true
+	if is_playing:
+		if countTMP>0:
+			countTMP = countTMP -1;
+		if countTMP == 0 :
+			nextNiveau();
+			countTMP = -1;
+		
+		Vaisseau_on_screen = (get_node("NiveauTemplate").get_node("Vaisseau")).is_on_screen()
+		if not Vaisseau_on_screen:
+			gameOver()
 	pass;
 
 func loadNiveau():
--- source/source/Vaisseau/Vaisseau.gd
+++ source/source/Vaisseau/Vaisseau.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 
 var is_moving = false;
 var velocity = Vector2.ZERO;
-var speed = 20;
+var speed = 10;
 var on_screen=true;
 
 # Called when the node enters the scene tree for the first time.
@@ -11,15 +11,15 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
-	if Input.is_action_just_released("launch"):
-		launch(get_global_mouse_position())
-	if is_moving:
-		position = position + velocity
-	else:
-		look_at(get_global_mouse_position())
-	if not get_node("notifier").is_on_screen():
-		on_screen = false
-
+	if get_parent().get_parent().is_playing:
+		if Input.is_action_just_released("launch"):
+			launch(get_global_mouse_position())
+		if is_moving:
+			position = position + velocity
+		else:
+			look_at(get_global_mouse_position())
+		if not get_node("notifier").is_on_screen():
+			on_screen = false
 func launch(dest):
 	velocity = (dest - position).normalized() * speed;
 	is_moving = true
